6766be29e3cfb73045f1edfe754a8085138c3935
Markus Tiefenbacher <markus.tiefenbacher@blue-tomato.com>
2020-03-26 15:18:18 +0100

    First Commit

---
+++ PageUseIdAsName.module.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Processwire;
+
+class PageUseIdAsName extends WireData implements Module, ConfigurableModule
+{
+
+    public static function getModuleInfo()
+    {
+        return array(
+            'title' => 'PageUseIdAsName',
+            'class' => 'PageUseIdAsName',
+            'version' => 100,
+            'summary' => 'Overrides the name field (used for url) with the page-id and hides the field from the admin GUI',
+            'singular' => true,
+            'autoload' => true,
+            'requires' => [
+                'PHP>=7.0.0',
+                'ProcessWire>=3.0.148'
+            ]
+        );
+    }
+
+    public function init()
+    {
+        $this->pages->addHookBefore('save', $this, 'changePageName');
+        $this->addHookAfter('AdminTheme::getExtraMarkup', $this, 'hideNameField');
+    }
+
+    public function hideNameField(HookEvent $event)
+    {
+        $parts = $event->return;
+        $fieldName = 'page_use_id_as_name_templates';
+        $allowedTemplates = $this->get($fieldName);
+        $allowedTemplates = wire('templates')->find("id=" . implode('|', $allowedTemplates));
+        if ($allowedTemplates && $allowedTemplates->count() > 0) {
+            $cssSelectors = [];
+            foreach ($allowedTemplates as $template) {
+                array_push($cssSelectors, ".ProcessPageEdit-template-$template->name .InputfieldPageName");
+            }
+            $parts['head'] .= '<style type="text/css">' . implode(',', $cssSelectors) . ' { display: none; }</style>';
+        }
+        $event->return = $parts;
+    }
+
+    public function changePageName(HookEvent $event)
+    {
+        $page = $event->arguments[0];
+        if ($this->isAllowedTemplate($page->template)) {
+            $id = wire('sanitizer')->pageName($page->id);
+            if (method_exists($page->name, "setLanguageValue")) {
+                foreach (wire('languages') as $language) {
+                    if ($language->name == "default") {
+                        $page->name = $id;
+                    } else {
+                        $page->set("name.$language->id", $id);
+                    }
+                }
+            } else {
+                $page->name = $id;
+            }
+        }
+    }
+
+    public static function getModuleConfigInputfields(array $data)
+    {
+        $wrapper = new InputfieldWrapper();
+        $fieldName = 'page_use_id_as_name_templates';
+
+        $templates = [];
+        foreach (wire('templates') as $template) {
+            // dont add system templates
+            if ($template->flags == Template::flagSystem) continue;
+            $templates[$template->id] = $template->name;
+        }
+
+        $field = wire('modules')->get('InputfieldAsmSelect');
+        $field->label = __('Templates');
+        $field->description = __('Choose for which templates the rule should be applied.');
+        $field->attr('name', $fieldName);
+        $field->attr('value', isset($data[$fieldName]) ? $data[$fieldName] : null);
+        $field->addOptions($templates);
+        $wrapper->append($field);
+
+        return $wrapper;
+    }
+
+    protected function isAllowedTemplate(Template $template)
+    {
+        $fieldName = 'page_use_id_as_name_templates';
+        $allowedTemplates = $this->get($fieldName);
+        return in_array($template->id, $allowedTemplates);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# PageUseIdAsName
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![ProcessWire 3](https://img.shields.io/badge/ProcessWire-3.x-orange.svg)](https://github.com/processwire/processwire)
+
+This Module for [ProcessWire CMS/CMF](http://processwire.com/) will replace your `$page-name` with `$page->id` so you have pages like
+`http://www.youdomain.com/parent-page/3463/`
+
+## Installation
+
+1. Execute the following command in the root directory of your ProcessWire installation:
+
+```bash
+composer require blue-tomato/blue-tomato/page-use-id-as-name
+```
+
+2. ProcessWire will detect the module and list it in the backend's `Modules` > `Site` > `ProcessLogstash` section. Navigate there and install it.
+
+3. Chose your tempaltes where the name replacement should be applied in the Module Configuration Page
+
+## Support
+
+Please [open an issue](https://github.com/blue-tomato/PageUseIdAsName/issues/new) for support.
+
+## Contributing
+
+Create a branch on your fork, add commits to your fork, and open a pull request from your fork to this repository.
+
+To get better insights and onboard you on module implementation details just open a support issue. We'll get back to you asap.
+
+## Credits
+
+This module is made by people from Blue Tomato. If you want to read more about our work, follow us on https://dev.to/btdev
+
+## License
+
+Find all information about this module's license in the LICENCE.txt file.
